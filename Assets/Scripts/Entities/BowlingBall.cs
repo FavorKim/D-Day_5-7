7c00c25edbc5ada8bc271e9bfb9f56d6d83451c6
FavorKim <163379317+FavorKim@users.noreply.github.com>
2024-05-07 02:13:08 +0900

    feat : Add Horizontal Move
    던지기 전 좌우로 공의 위치를 움직이는 기능 추가.
    
    클릭한 후에는 움직일 수 없으나, 도중에 좌클릭을 떼면 다시 움직일 수 있음

--- Assets/Scripts/Entities/BowlingBall.cs
+++ Assets/Scripts/Entities/BowlingBall.cs
@@ -9,7 +9,7 @@ public class BowlingBall : MonoBehaviour
 {
 
     private Rigidbody rb;
-    
+
     [SerializeField] private PinsManager pMLeft;
     [SerializeField] private Transform centerOfLane; // 레인의 중간 지점
     [SerializeField, Tooltip("R키를 눌렀을 때 볼링공을 되돌릴 위치(Transform)")] private Transform startPos; // 시작 지점
@@ -19,6 +19,9 @@ public class BowlingBall : MonoBehaviour
     private float maxBowlPow = 1.0f;
     private float finalPow;
 
+    [SerializeField] private float moveSpeed;
+
+
     [SerializeField, Tooltip("볼링공 속도 계수")] private float bowlPower;
     [SerializeField, Tooltip("백스윙 지속시 파워 감소량")] private float powerMinusPerSec;
     [SerializeField, Tooltip("백스윙시 최대 파워 증가량")] private float powerPlusforBackSwing;
@@ -27,9 +30,12 @@ public class BowlingBall : MonoBehaviour
     [SerializeField, Tooltip("백스윙 지속 시간")] private float backswingPersistence = 0;
     [SerializeField, Tooltip("백스윙 지속 시 감속을 시작할 시간")] private float timeToStartDecrease;
 
-    private bool corStarted = false;
-    private bool isRolling = false;
 
+    private bool corStarted = false;
+    [SerializeField] private bool isRolling = false;
+    [SerializeField] private bool isMoving = true;
+
+    private Vector2 moveDir;
     private Vector2 dir;
 
     private void Start()
@@ -46,6 +52,22 @@ public class BowlingBall : MonoBehaviour
     private void Update()
     {
         Bowl();
+        Move();
+    }
+
+    public void OnMove(InputValue val)
+    {
+        moveDir = val.Get<Vector2>().normalized;
+    }
+
+    void Move()
+    {
+        if (!isMoving) return;
+        if (transform.position.x > 5.8 && moveDir == Vector2.left)
+            return;
+        if (transform.position.x < 4.5 && moveDir == Vector2.right)
+            return;
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime);
     }
 
     void Bowl()
@@ -56,12 +78,14 @@ public class BowlingBall : MonoBehaviour
          * 
          * 1. 공을 굴릴 수 있는 상황을 제한할 것.
          *  - 예를 들면, 공이 굴러가는 도중에 공을 추가로 굴릴 수 없어야 하며, 설정 UI을 열었을 때 공을 굴릴 수 없어야 한다.
-         *  
-         * 2. 
+         *   
+         *   Confirmed 
+         * 
          */
 
         if (Input.GetMouseButton(0))       // 마우스를 누르고 있을 때
         {
+            isMoving = false;
             if (backswingPersistence > timeToStartDecrease)         // 백스윙을 오래지속했을 경우
                 maxBowlPow -= powerMinusPerSec * Time.deltaTime;    // 최대 파워 감소
             else if (pow < 0)
@@ -71,7 +95,7 @@ public class BowlingBall : MonoBehaviour
                 backswingPersistence += Time.deltaTime;
             }
 
-            if (pow > 0)
+            if (pow > 0.0f)
                 SetFinalMax(pow);         // 마우스의 델타값이 양수(왼쪽)일 때, delta 값을 구하여 최대치를 갱신
 
 
@@ -79,24 +103,33 @@ public class BowlingBall : MonoBehaviour
                 finalPow = maxBowlPow;
         }
 
-        if (Input.GetMouseButtonUp(0)&&!isRolling)     // 마우스를 뗄 때 (스윙을 끝내고 투척할 때)
+        if (Input.GetMouseButtonUp(0) && !isRolling)     // 마우스를 뗄 때 (스윙을 끝내고 투척할 때)
         {
-            
 
-            // 갱신된 최대 파워 만큼 공의 앞 방향으로 볼링공 투척
-            rb.AddForce(transform.forward * finalPow * Time.deltaTime * bowlPower, ForceMode.Impulse);
 
-            // 마우스 뗐을 때의 위 아래 입력에 따라 볼링공을 회전시킴
-            rb.angularVelocity = new Vector3(-dir.y * spinPower, rb.angularVelocity.y, dir.x * spinPower) * Time.deltaTime;
+
+            if (finalPow < 10.0f)
+            {
+                Debug.Log("던지는 힘이 너무 작아 리셋");
+                ResetBall();
+            }
+            else
+            {
+                isRolling = true;
+                // 갱신된 최대 파워 만큼 공의 앞 방향으로 볼링공 투척
+                rb.AddForce(transform.forward * finalPow * Time.deltaTime * bowlPower, ForceMode.Impulse);
+                // 마우스 뗐을 때의 위 아래 입력에 따라 볼링공을 회전시킴
+                rb.angularVelocity = new Vector3(-dir.y * spinPower, rb.angularVelocity.y, dir.x * spinPower) * Time.deltaTime;
+            }
+
 
             // 투척 완료시 최대 파워, 현재 파워 초기화
 
             //Debug.Log("Final : " + finalPow);
             //Debug.Log("Max : " + maxBowlPow);
-            finalPow = 0.1f;
+            finalPow = 0.0f;
             maxBowlPow = 1.0f;
             backswingPersistence = 0;
-            isRolling = true;
         }
     }
 
@@ -124,6 +157,7 @@ public class BowlingBall : MonoBehaviour
         transform.position = startPos.position;
         transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
         isRolling = false;
+        isMoving = true;
     }
 
 
@@ -143,6 +177,7 @@ public class BowlingBall : MonoBehaviour
             yield return new WaitForSeconds(3.0f);
             GameManager.Instance.FloorSet();
             isRolling = false;
+            isMoving = true;
             corStarted = false;
             StopCoroutine(CorEndFloor());
             break;

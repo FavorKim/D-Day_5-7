44ff40c425f4a2f3ed5f5dbbf6df6427238b63ea
jeongseo7 <seowol272912@gmail.com>
2024-05-07 13:02:51 +0900

    All

---
+++ Assets/UI/Scripts/ScoreUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+
+
+    List<Text[]> TotalScore = new List<Text[]>();
+
+
+    [SerializeField]
+    //ÇÉÀÇ °¹¼ö Ç¥½Ã
+    Text[] PinScore;
+
+    [SerializeField]
+    //ÃÑ Á¡¼ö
+    Text[] Score;
+
+    
+
+  
+
+}
